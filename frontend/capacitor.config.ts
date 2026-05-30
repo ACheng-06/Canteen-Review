b22b4fc70cdd2183c6763a7d34adca4c7c4d6149
JinCheng-06 <2011024577@qq.com>
2026-05-30 22:27:55 +0800

    测试版1.0

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.canteenreview.app',
   appName: 'Canteen Review',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+  },
 };
 
 export default config;
